abd6e939af02ee307024a7bffe5af1a2e317493d
Kseniya Kuzmina <kuzminaksyushka@gmail.com>
2022-08-29 18:40:04 +0400

    Update example

--- CS/Series_LineChart/Form1.cs
+++ CS/Series_LineChart/Form1.cs
@@ -1,6 +1,7 @@
 using System;
 using System.Windows.Forms;
 using DevExpress.XtraCharts;
+using System.Drawing;
 // ...
 
 namespace Series_LineChart {
@@ -15,7 +16,7 @@ namespace Series_LineChart {
 
             // Create a line series.
             Series series1 = new Series("Series 1", ViewType.Line);
-            
+
             // Add points to it.
             series1.Points.Add(new SeriesPoint(1, 2));
             series1.Points.Add(new SeriesPoint(2, 12));
@@ -30,18 +31,23 @@ namespace Series_LineChart {
             series1.ArgumentScaleType = ScaleType.Numerical;
 
             // Access the view-type-specific options of the series.
+            ((LineSeriesView)series1.View).MarkerVisibility = DevExpress.Utils.DefaultBoolean.True;
+            ((LineSeriesView)series1.View).LineMarkerOptions.Size = 20;
             ((LineSeriesView)series1.View).LineMarkerOptions.Kind = MarkerKind.Triangle;
             ((LineSeriesView)series1.View).LineStyle.DashStyle = DashStyle.Dash;
 
             // Access the view-type-specific options of the series.
-            ((XYDiagram)lineChart.Diagram).Rotated = true;
+            ((XYDiagram)lineChart.Diagram).AxisY.Interlaced = true;
+            ((XYDiagram)lineChart.Diagram).AxisY.InterlacedColor = Color.FromArgb(20, 60, 60, 60);
+            ((XYDiagram)lineChart.Diagram).AxisX.NumericScaleOptions.AutoGrid = false;
+            ((XYDiagram)lineChart.Diagram).AxisX.NumericScaleOptions.GridSpacing = 1;
 
             // Hide the legend (if necessary).
             lineChart.Legend.Visibility = DevExpress.Utils.DefaultBoolean.False;
 
             // Add a title to the chart (if necessary).
             lineChart.Titles.Add(new ChartTitle());
-            lineChart.Titles[0].Text = "A Line Chart";
+            lineChart.Titles[0].Text = "Line Chart";
 
             // Add the chart to the form.
             lineChart.Dock = DockStyle.Fill;
